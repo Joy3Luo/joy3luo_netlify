6080e66b62564ff5879988002b45d8aa6e87c85e
Joy3Luo <67431909+Joy3Luo@users.noreply.github.com>
2020-12-22 05:32:03 -0500

    u

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,22 @@ import SectionContent from './SectionContent';
 import SectionForm from './SectionForm';
 import SectionHero from './SectionHero';
 import SectionPosts from './SectionPosts';
+
+
+import Action from './Action';
+import BlogFeedItemFilter from './BlogFeedItemFilter';
+import BlogFeedSection from './BlogFeedSection';
+import BlogPostCategories from './BlogPostCategories';
+import BlogPostFeedItem from './BlogPostFeedItem';
+import BlogPostTags from './BlogPostTags';
+import ContactSection from './ContactSection';
+import ContentSection from './ContentSection';
+import CtaSection from './CtaSection';
+import FeaturesSection from './FeaturesSection';
+import HeroSection from './HeroSection';
+import SectionActions from './SectionActions';
+import TeamSection from './TeamSection';
+
 import Layout from './Layout';
 
 export {
@@ -17,6 +33,21 @@ export {
     SectionForm,
     SectionHero,
     SectionPosts,
+
+    Action,
+    BlogFeedItemFilter,
+    BlogFeedSection,
+    BlogPostCategories,
+    BlogPostFeedItem,
+    BlogPostTags,
+    ContactSection,
+    ContentSection,
+    CtaSection,
+    FeaturesSection,
+    HeroSection,
+    SectionActions,
+    TeamSection,
+
     Layout
 };
 
@@ -29,5 +60,20 @@ export default {
     SectionForm,
     SectionHero,
     SectionPosts,
+
+    Action,
+    BlogFeedItemFilter,
+    BlogFeedSection,
+    BlogPostCategories,
+    BlogPostFeedItem,
+    BlogPostTags,
+    ContactSection,
+    ContentSection,
+    CtaSection,
+    FeaturesSection,
+    HeroSection,
+    SectionActions,
+    TeamSection,
+
     Layout
 };
